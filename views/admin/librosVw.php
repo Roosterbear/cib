877ce3ecc380b3694002576f53501f293093402b
Roosterbear <fernandoroosterbear@gmail.com>
2024-02-22 15:29:53 -0600

    casi no hice nada, pero ahi va la vista de ABC

--- views/admin/librosVw.php
+++ views/admin/librosVw.php
@@ -1,32 +1,29 @@
+<h2>No me veas asi, estoy en construccion...</h2>
+<br/>
 <div class="container-fluid">
 
 
-  <!-- Captura de TITULO del Libro -->
   <div class="row areaCaptura">
-    <div class="col-md-2"></div>
-		<div class="col-md-8">
+  	<!-- Captura de TITULO del Libro -->
+    <div class="col-md-1"></div>
+		<div class="col-md-4">
 			<i class="fa fa-address-book iconoCaptura"></i>
 			<label class="labelCaptura">Titulo:</label>
 			<input name="inputTituloLibro" class="form-control inputBuscar" id="inputTituloLibro" onpaste="return false"/>
 		</div>
-  </div>
-
-
-  <!-- Captura de AUTOR del Libro -->
-  <div class="row areaCaptura">
-    <div class="col-md-2"></div>
-		<div class="col-md-8">
+  	<!-- Captura de AUTOR del Libro -->
+    <div class="col-md-1"></div>
+		<div class="col-md-4">
 			<i class="fa fa-user iconoCaptura"></i>
 			<label class="labelCaptura">Autor:</label>
 			<input name="inputAutorLibro" class="form-control inputBuscar" id="inputAutorLibro" onpaste="return false"/>
 		</div>
   </div>
 
-
-  <!-- Captura de ISBN del Libro -->
   <div class="row areaCaptura">
-    <div class="col-md-2"></div>
-		<div class="col-md-8">
+  	<!-- Captura de ISBN del Libro -->
+    <div class="col-md-1"></div>
+		<div class="col-md-4">
 			<i class="fa fa-archive iconoCaptura"></i>
 			<label class="labelCaptura">ISBN:</label>
 			<input name="inputISBN" class="form-control inputBuscar" id="inputISBN" onpaste="return false"/>
@@ -34,15 +31,13 @@
   </div>
 
 
-  <!-- Captura de Clasificacion del Libro -->
-  <div class="row areaCaptura">
-    <div class="col-md-2"></div>
-		<div class="col-md-8">
+	  <!-- Captura de Clasificacion del Libro -->
+    <div class="col-md-1"></div>
+		<div class="col-md-4">
 			<i class="fa fa-tag iconoCaptura"></i>
 			<label class="labelCaptura">Clasificacion:</label>
 			<input name="inputClasificacion" class="form-control inputBuscar" id="inputClasificacion" onpaste="return false"/>
 		</div>
-  </div>
 
 
 
