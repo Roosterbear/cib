d974547a4bf59d0e07164ac4014667e5da67d32a
Roosterbear <fernandoroosterbear@gmail.com>
2024-03-07 09:06:18 -0600

    listo las vistas basicas, ahora si comencemos la funcionalidad

--- views/admin/cambioEjemplarVw.php
+++ views/admin/cambioEjemplarVw.php
@@ -1,3 +1,40 @@
+<!-- ******************************************** -->
+<!-- ****  BUSQUEDA DE EJEMPLAR PARA CAMBIO  **** -->
+<!-- ******************************************** -->
+<div id="busquedaCambioEjemplar">
+	<div class="container-fluid">
+    <div class="row areaCaptura">
+			<div class="col-md-1"></div>
+			<div class="col-md-10">
+		   	<i class="fa fa-search iconoBuscar"></i>
+			  <i class="fa fa-book iconoBuscar"></i>
+			  <label class="labelCaptura" for="inputBuscarPorAdquisicionCambioEjemplar">Por No. de Adquisici&oacute;n:</label>
+			  <input name="inputBuscarPorAdquisicionCambioEjemplar" class="form-control inputBuscar" id="inputBuscarPorAdquisicionCambioEjemplar" onpaste="return false"/>
+		  </div>
+			<div class="col-md-1"></div>
+	  </div><!-- row  -->
+    
+    <div class="row areaCaptura">
+			<div class="col-md-1"></div>
+      <!-- Busqueda de Ficha por Titulo -->
+			<div class="col-md-5">
+		   	<i class="fa fa-search iconoBuscar"></i>
+			  <i class="fa fa-book iconoBuscar"></i>
+			  <label class="labelCaptura" for="inputBuscarPorTituloCambioEjemplar">Por Titulo:</label>
+			  <input name="inputBuscarPorTituloCambioEjemplar" class="form-control inputBuscar" id="inputBuscarPorTituloCambioEjemplar" onpaste="return false"/>
+		   </div>
+			
+      <!-- Busqueda de Ficha por Autor -->
+		  <div class="col-md-5">
+			  <i class="fa fa-search iconoBuscar"></i>
+			  <i class="fa fa-user-circle iconoBuscar"></i>
+			  <label class="labelBuscar" for="inputBuscarPorAutorCambioEjemplar">Por Autor:</label>
+			  <input name="inputBuscarAutorCambioEjemplar" class="form-control inputBuscar" id="inputBuscarAutorCambioEjemplar" onpaste="return false"/>
+		   </div>
+			<div class="col-md-1"></div>
+   <div id="contenidoData"></div>
+  </div>
+</div>
 <h1>Cambio de Ejemplar</h1>
 <small>Aqui se va a mostrar los mismo que en bajas solo que cambia a icono de editar</small>
 <br/>
