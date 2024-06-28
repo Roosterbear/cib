23b3611a42c6f6bbdb72bb67ce2e5455cc810e21
Roosterbear <fernandoroosterbear@gmail.com>
2024-06-28 14:25:20 -0600

    ya esta el detalle de ficha

--- views/admin/detalleFicheroVw.php
+++ views/admin/detalleFicheroVw.php
@@ -1,11 +1,40 @@
-<h1><?php echo $id; ?></h1>
-<h1><?php echo $titulo; ?></h1>
-<h1><?php echo $autor; ?></h1>
-<h1><?php echo $isbn; ?></h1>
-<h1><?php echo $clasificacion; ?></h1>
-<hr>
-<?php
-foreach($ejemplar as $e){
-  echo "<h3>".$e['adq']." - ".$e['volumen']." - ".$e['tomo']." - ".$e['accesible']."</h3>";
-}
-?>
+<div class="container">
+  <div class="row ficha">
+    <div class="col-md-11">
+      <h1 class="fichero"><?php echo $titulo; ?></h1>
+    </div>
+    <div class="col-md-1 id">ID: <u><?php echo $id; ?></u></div>
+    <div class="col-md-12">
+      <h2>Autor: <?php echo $autor; ?></h2>
+    </div>
+    <div class="col-md-12">
+      <h3>ISBN: <?php echo $isbn; ?></h3>
+    </div>
+    <div class="col-md-12">
+      <h3>Clasificaci&oacute;n: <?php echo $clasificacion; ?></h1>
+    </div>  
+    <div class="col-md-12"><hr/></div>
+    <div class="col-md-12">
+      <table class="cib-table">
+        <thead>
+          <th class="text-center"> ADQUISICION </th>
+          <th class="text-center"> VOLUMEN </th>
+          <th class="text-center"> TOMO </th>
+          <th class="text-center"> ACCESIBLE </th>
+        </thead>
+        <tbody>
+        <?php
+          foreach($ejemplar as $e){
+            echo "<tr>";
+            echo "<td class=\"text-center\">".$e['adq']."</td>";
+            echo "<td class=\"text-center\">".$e['volumen']."</td>";
+            echo "<td class=\"text-center\">".$e['tomo']."</td>";
+            echo "<td class=\"text-center\">".$e['accesible']."</td>";
+            echo "</tr>";
+          }
+          ?>
+        </tbody>  
+      </table>
+    </div>
+  </div>
+</div>
